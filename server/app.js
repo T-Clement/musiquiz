4e6475c1ca9a4a8e59a2b4cf947e793858ed7cd7
T-Clement <toquet.clement@gmail.com>
2024-08-03 15:33:47 +0200

    add first validator with express-validator

--- server/app.js
+++ server/app.js
@@ -4,15 +4,40 @@ const express = require('express');
 
 // const mongoose = require('mongoose');
 
+const { body, validationResult } = require("express-validator");
+
+
+
 const userRoutes = require('./routes/user');
 
-  
- 
+
+
+
+
+// validation middleware  
+const validateLogin = [
+    body('pseudo')
+        .trim()
+        .escape()
+        .isString().withMessage('Le pseudo doit être une chaîne de caractères')
+        .notEmpty().withMessage('Le pseudo est requis'),
+    body('password')
+        .trim()
+        .escape()
+        .isString().withMessage('Le mot de passe doit être une chaîne de caractères')
+        .notEmpty().withMessage('Le mot de passe est requis'),
+];
+
+
+
+
+
+
 
 const app = express();
 
 
-
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 
@@ -27,10 +52,32 @@ app.use((req, res, next) => { // middleware pour le CORS
 // routes
 app.use('/api/user/', userRoutes);
 
+app.post('/api/login', validateLogin, (req, res, next) => {
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors : errors.array() })
+    }
+
+    // check if user and password field are in req
+
+    // data coming from request
+    
+    // console.log(req.body);
+    
+    
+    const { username, password } = req.body;
+
+
+    res.status(200).json({ message : "data received" });
+
+});
+
 
 // 404 route to place at the end
 app.get('*', (req, res, next) => {
-    res.status(404).json({message : "404, ressource not found"})
+    res.status(404).json({ message : "404, ressource not found" })
 })
 
 
