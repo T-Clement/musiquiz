b6091f095efadcf5f92c0df54255808aaeed263c
T-Clement <toquet.clement@gmail.com>
2024-11-01 16:39:17 +0100

    add beginning of rendering for players list in presentator waitingroom view

--- front/src/pages/WaitingRoom/WaitingRoomPage.jsx
+++ front/src/pages/WaitingRoom/WaitingRoomPage.jsx
@@ -201,14 +201,17 @@ export function WaitingRoomPage() {
           <WaitingRoomPlayer socket={socket} presentator={presentator}/>
       }
 
+      <div className='flex justify-center'>
 
-      <button
-        type="button"
-        onClick={() => handleQuitRoom(userId, role)}
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-      >
-        Quitter
-      </button>
+        <button
+          type="button"
+          onClick={() => handleQuitRoom(userId, role)}
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          Quitter
+        </button>
+
+      </div>
 
 
     </div>
--- front/src/pages/WaitingRoom/WaitingRoomPresentator.jsx
+++ front/src/pages/WaitingRoom/WaitingRoomPresentator.jsx
@@ -3,30 +3,56 @@ import React from 'react'
 export default function WaitingRoomPresentator({players, presentator, socket}) {
     return (
         <div>
-            <h2>Joueurs dans la salle :</h2>
-            {players && players.length > 0 ? (
-                <ul>
-                    {players.map(player => (
-                        <li key={player.userId}>{player.pseudo} {player.socketId === socket.id ? "(You)" : ""} - {player.socketId}</li>
-                    ))}
-                </ul>
-            ) : (
-                <p>Aucun joueur pour le moment</p>
-            )}
-
-
             <div className='flex gap-2'>
                 <h2>Présentateur :</h2>
                 <input type='checkbox' checked={presentator != null} readOnly />
             </div>
+            
+            
+
+            <div className='flex mb-4 justify-around'>
+
+                <div className='w-1/2'>
+
+                    <h2 className='mb-2'>Joueurs dans la salle : <span className='ms-3 px-3 py-1 rounded-lg bg-white text-black font-bold'>{players.length}</span></h2>
+                    
+                    {players && players.length > 0 ? (
+                        <ul className='h-80 overflow-y-scroll grid grid-cols-4 border border-white'>
+                            {players.map(player => (
+                                <li key={player.userId} className='flex flex-col items-center gap-2 p-2'>
+                                    <img src='/public/assets/circle-user-solid.svg' style={{width: '32px'}}/>
+                                    <p>{player.pseudo} {/*player.socketId === socket.id ? "(You)" : ""*/}</p>
+                                    <button type='button'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill='white' viewBox="0 0 512 512" width={24}>
+                                            <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/>
+                                        </svg>
+                                    </button>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>Aucun joueur pour le moment</p>
+                    )}
+
+                </div>
 
 
-            <button 
-                className='bg-green-600 px-5 py-2.5 rounded-lg disabled:bg-green-300 disabled:cursor-not-allowed'
-                disabled={! players.length > 0}
-            >
-                Lancer la partie
-            </button>
+
+                <div className='flex items-center justify-items-center'>
+                    
+                    <button 
+                        className='bg-green-600 px-5 py-2.5 rounded-lg disabled:bg-green-300 disabled:cursor-not-allowed'
+                        disabled={! players.length > 0}
+                    >
+                        Lancer la partie
+                    </button>
+
+                </div>
+
+
+
+            </div>
+
 
 
 
