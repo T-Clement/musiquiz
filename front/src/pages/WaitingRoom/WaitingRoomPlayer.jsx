1896a2c665c2c039d9815dc47d1565e739f51d9d
T-Clement <toquet.clement@gmail.com>
2024-11-01 15:52:07 +0100

    add readOnly attribute to checkbox

--- front/src/pages/WaitingRoom/WaitingRoomPlayer.jsx
+++ front/src/pages/WaitingRoom/WaitingRoomPlayer.jsx
@@ -7,7 +7,7 @@ export default function WaitingRoomPlayer({socket, presentator}) {
 
   return (
     <div className='max-w-sm mx-auto my-8 flex flex-col gap-6 items-center'>
-        <label>Présentateur : <input type='checkbox' checked={presentator} /></label>
+        <label>Présentateur : <input type='checkbox' checked={presentator != null} readOnly/></label>
         <h2 className='text-center font-bold text-xl px-10'>
             {presentator ? "En attente du lancement de la partie par le présentateur" :
             "En attente d'un présentateur pour la partie"
