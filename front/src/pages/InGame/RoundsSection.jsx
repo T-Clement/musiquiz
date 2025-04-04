4bcf2bf5abdbd90feaa0ec591f8c0566ea4bb391
T-Clement <toquet.clement@gmail.com>
2025-04-04 22:48:44 +0200

    add style and display correct data in rounds list items

--- front/src/pages/InGame/RoundsSection.jsx
+++ front/src/pages/InGame/RoundsSection.jsx
@@ -1,34 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function RoundsSection({rounds}) {
-  
+import svgPlay from "/assets/play.svg";
+
+export default function RoundsSection({ rounds }) {
   console.log(rounds);
-return (
-
-    <ul role="list" className="divide-y divide-gray-100 bg-slate-600 rounded-lg">
-        {rounds.map(round => 
-            (
-            <li className="flex flex-col justify-between gap-x-6 px-3 py-5 min-w-64 md:min-w-[420px]">
-                <div className="flex min-w-0 gap-x-4">
-                {/* <img className="size-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/> */}
-                <div className="min-w-0 flex-auto">
-                    <p className="text-sm/6 font-semibold text-slate-50">{ round.artist }</p>
-                    <p className="mt-1 truncate text-xs/5 text-slate-50">leslie.alexander@example.com</p>
-                    <p className='mt-1 text-xs/5 flex justify-end bg-b' ><a target="_blank" className="bg-blue-900 underline p-2 text-slate-50">Ecouter l'extrait</a></p>
-                </div>
-                </div>
-                <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                {/* <p className="text-sm/6 text-gray-900">Co-Founder / CEO</p>
-                <p className="mt-1 text-xs/5 text-gray-500">Last seen <time datetime="2023-01-23T13:23Z">3h ago</time></p> */}
-                </div>
-            </li>
-
-            )
-        )}
-        
+  return (
+    <ul
+      role="list"
+      className="divide-y divide-gray-100 bg-slate-600 rounded-lg"
+    >
+      {rounds.map((round) => (
+        <li className="flex flex-col justify-between gap-x-6 px-6 py-5 min-w-64 md:min-w-[420px]">
+          <div className="flex justify-between min-w-0 gap-x-4">
+            <div className="flex-col self-center">
+              <p className="font-semibold text-slate-50">{round.artist}</p>
+              <p className="mt-1 truncate text-slate-50 italic">{round.title}</p>
+            </div>
+            <a
+              target="_blank"
+              className="p-4 text-slate-50 relative text-xs/5 flex flex-col items-center "
+              href={round.songExtract}
+            >
+              <img
+                src={svgPlay}
+                alt="Play Icon"
+                className="relative z-10 ps-1 w-8 h-8 fill-current text-white"
+                />
+                <p>Ecouter l'extrait</p>
+              <span className="absolute inset-0 rounded-full bg-white opacity-25"></span>
+            </a>
+          </div>
+          <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end"></div>
+        </li>
+      ))}
     </ul>
-
-
-
-    )
+  );
 }
