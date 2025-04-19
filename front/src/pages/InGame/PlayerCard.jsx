c78219143fc93aa16fcd59c6abaf451286bb1b0e
T-Clement <toquet.clement@gmail.com>
2025-04-19 18:51:43 +0200

    update poduim component

--- front/src/pages/InGame/PlayerCard.jsx
+++ front/src/pages/InGame/PlayerCard.jsx
@@ -1,20 +1,57 @@
 import React from "react";
 
 export default function PlayerCard({ player, index, totalPlayers }) {
-  let sizeClass;
-  if (totalPlayers === 1) {
-    sizeClass = "w-40 h-52 md:w-48 md:h-60";
-  } else if (totalPlayers === 2) {
-    sizeClass = "w-36 h-44 md:w-44 md:h-52";
-  } else {
-    sizeClass = index === 0 ? "w-32 h-40 md:w-40 md:h-52" :
-                index === 1 ? "w-32 h-32 md:w-36 md:h-40" :
-                "w-32 h-28 md:w-32 md:h-33";
-  }
+
+    // size of tile related to number of players in the game
+    // and also the final ranking of the player
+    let sizeClass;
+    if (totalPlayers === 1) {
+      sizeClass = "w-40 h-52 md:w-48 md:h-60";
+    } else if (totalPlayers === 2) {
+
+      // 2 players : first bigger and second smaller
+      sizeClass =
+        index === 0
+          ? "w-36 h-44 md:w-44 md:h-52"
+          : "w-32 h-32 md:w-36 md:h-40";
+    } else {
+
+      // 3+ players : podium first, 2nd, 3rd with differents sizes
+      sizeClass =
+        index === 0
+          ? "w-32 h-40 md:w-40 md:h-52"  // first
+          : index === 1
+          ? "w-32 h-32 md:w-36 md:h-40"  // 2nd
+          : "w-32 h-28 md:w-32 md:h-33"; // 3rd
+    }
+  
+    // order for podium
+    let orderClass;
+    if (totalPlayers === 1) {
+      // one player, in the center
+      orderClass = "order-1";
+    } else if (totalPlayers === 2) {
+      // 2 players: order first and second
+      orderClass = `order-${index + 1}`;
+    } else {
+      // 3+ joue
+      // order 3 - 1 - 2
+      switch (index) {
+        case 2:
+          orderClass = "order-1"; // third player at the left
+          break;
+        case 0:
+          orderClass = "order-2"; // first player in the middle
+          break;
+        case 1:
+          orderClass = "order-3"; // second player on the right
+          break;
+
+      }
+    }
 
   return (
-    <div className={`relative flex flex-col items-center bg-white rounded-lg shadow-md p-4 ${sizeClass} ${
-        index === 0 ? "order-2" : index === 1 ? "order-1" : "order-3"
+    <div className={`relative flex flex-col items-center bg-white rounded-lg shadow-md p-4 ${sizeClass} ${orderClass}
       }`}>
       <img
         src={""}
