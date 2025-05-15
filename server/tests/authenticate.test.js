6a988aa454b85899ddfda0a396366a27e39efac5
T-Clement <toquet.clement@gmail.com>
2025-05-15 12:12:27 +0200

    add simple unit test on jwt token encoded values

--- server/tests/authenticate.test.js
+++ server/tests/authenticate.test.js
@@ -2,8 +2,29 @@ const request = require('supertest');
 const app = require('../app');
 const jwt = require('jsonwebtoken');
 
+const utils = require("../utils/utils");
+
+describe("unit tests on jwt token functions", () => {
+    
+    
+    test("generateAccessToken encode correctly values", async () => {
+        const user = { id : 99, pseudo: "John" };
+        const secret = "jwt-secret";
+
+        const token = await utils.generateAccessToken(user, secret,'1h');
+        const payload = jwt.verify(token, secret);
+        console.log(payload, user)
+        expect(payload).toMatchObject({ userId: user.id, pseudo: user.pseudo });
+    });
+
+
+});
+
+
 
 describe('authenticateJWT', () => {
+
+
     it("returns 401 when no token", async () => {
         const res = await request(app).get("/api/me");
         expect(res.statusCode).toBe(401);
