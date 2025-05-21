76acba067037a2dd00ad467745e7c7e4b5c617f9
T-Clement <toquet.clement@gmail.com>
2025-05-21 20:32:41 +0200

    [REFACTO] update autenticateJWT middleware by exploding it in multiple functions

--- server/middleware/Auth.js
+++ server/middleware/Auth.js
@@ -1,59 +1,85 @@
 const jwt = require("jsonwebtoken");
 
-exports.authenticateJWT = async (req, res, next) => {
-  // console.log("in authenticate middleware");
-  // console.log("log of list of cookies");
-  // console.log(req.cookies);
-  const token = req.cookies.accessToken;
-  console.log(token);
-  if (!token) {
-    // console.log("Dans le if !token");
-    // si pas de token, essayer de renouveler avec le refresh token
-    const refreshToken = req.cookies.refreshToken;
-    if (!refreshToken) {
-      return res.status(401).json({ message: "Non authentifié" });
-    }
-    // is it working here ???
-    jwt.verify(
-      refreshToken,
-      process.env.JWT_REFRESH_SECRET_KEY,
-      (err, user) => {
-        // user in payload of token
-        if (err) {
-          return res
-            .status(403)
-            .json({ message: "Refresh token invalide ou expiré" });
-        }
+const { generateAccessToken } = require("../utils/utils");
 
-        const utils = require("../utils/utils");
-        // generate a new accessToken
-        const newAccessToken = utils.generateAccessToken(
-          { userId: user.userId, pseudo: user.pseudo },
-          process.env.JWT_SECRET_KEY,
-          "15m"
-        );
-        res.cookie("accessToken", newAccessToken, {
-          httpOnly: true,
+async function handleRefreshToken(req, res, next) {
+  const refreshToken = req.cookies.refreshToken;
+  if (!refreshToken) {
+    return res.status(401).json({ message: "Non authentifié" });
+  }
+  // verify refresh token
+  try {
+    const payload = await verifyJwt(refreshToken, process.env.JWT_REFRESH_SECRET_KEY);
+
+    const newAccessToken = generateAccessToken(
+      { userId: payload.userId, pseudo: payload.pseudo },
+      process.env.JWT_SECRET_KEY,
+      "15m"
+    );
+
+    res.cookie("accessToken", newAccessToken, {
+      httpOnly: true,
           secure: process.env.NODE_ENV === "production",
           sameSite: "Strict",
           maxAge: 15 * 60 * 1000, // 15 minutes validation
-        });
-        // console.log("accessToken regenerated due to valid refreshToken");
-        req.user = { userId: user.userId, pseudo: user.pseudo };
-        next();
-        // envoyer les infos utilisateur
-        // return res.status(200).json({ user: { userId: user.userId, pseudo: user.pseudo } });
-      }
-    );
-  } else {
-    // if accessToken is valid
-    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
-      if (err) {
-        return res.status(403).json({ message: "Token expiré ou invalide" });
-      }
-      req.user = { userId: user.userId, pseudo: user.pseudo };
-      next();
     });
+
+    // add user data to request
+    req.user = { userId: payload.userId, pseudo: payload.pseudo };
+    
+    next();
+  } catch (err) { // for any error occured
+    return res.status(401).json({ message: "Refresh token invalide ou expiré" });
+  }
+}
+
+
+function verifyJwt(token, secret) {
+  // return a promise that resolves with the payload or rejects with an error
+  return new Promise((resolve, reject) => {
+    // verify the token with the secret key
+    jwt.verify(token, secret, (err, payload) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(payload);
+    });
+  });
+}
+
+/**
+ * Middleware to authenticate JWT token
+ * 
+ * Check if accessToken is in cookies of request
+ * If not or expired, try to refresh it with refreshToken
+ * Add user data to request (req.user)
+ * Return 401 if error
+ * 
+ * @param {import('express').Request} req 
+ * @param {import('express').Response} res 
+ * @param {Function} next 
+ * @returns 
+ */
+exports.authenticateJWT = async (req, res, next) => {
+  try {
+    
+    const token = req.cookies.accessToken;
+    
+    // no access token, try to refresh it
+    if(!token) return await handleRefreshToken(req, res, next);
+
+    // access token is present, verify it
+    const payload = await verifyJwt(token, process.env.JWT_SECRET_KEY);
+    
+    // add user data to request
+    req.user = { userId: payload.userId, pseudo: payload.pseudo };
+    
+    // continue to next middleware
+    next();
+
+  } catch (err) {
+    // if any error occured, send 401
+    return res.sendStatus(401);
   }
 };
 
