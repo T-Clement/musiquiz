38ca7feb2aadb2f15711249ca7223efd47355b54
T-Clement <toquet.clement@gmail.com>
2025-06-07 14:18:25 +0200

    add last games played to AccountPage + [FIX] fix key typo in GameEngine key of room in #endGame

--- front/src/pages/Account/LastGamesTimeline.jsx
+++ front/src/pages/Account/LastGamesTimeline.jsx
@@ -1,4 +1,5 @@
 import Button from "../../components/Button";
+import Heading2 from "../../components/Heading2";
 
 export function LastGamesTimeline({ games }) {
   return (
@@ -8,33 +9,40 @@ export function LastGamesTimeline({ games }) {
         className="absolute left-0 top-0 h-full w-1
                        bg-slate-50 rounded"
       />
-      <h3 className="text-xl font-semibold text-white mb-8">
+      <Heading2 additionnalClasses="text-xl font-semibold text-white mb-4">
         Vos dernières parties
-      </h3>
+      </Heading2>
 
-      <ul className="space-y-10">
+      <ul className="space-y-6">
         {games.map((gameData, index) => (
-          <li key={gameData.game.id} className="relative">
+          <li
+            key={gameData.game.id}
+            className="relative flex flex-col md:flex-row md:justify-between gap-2 rounded-lg bg-slate-800/70 backdrop-blur
+                            ring-1 ring-white/10 shadow  hover:shadow-purple-700 transition
+                            px-5 py-4 "
+          >
             <div
-              className="rounded-lg bg-slate-800/70 backdrop-blur
-                            ring-1 ring-white/10 shadow
-                            px-5 py-4 flex flex-col gap-2
-                            hover:shadow-purple-700 transition"
+              className="flex flex-col gap-2"
             >
+              <p className="text-lg font-black">{gameData.metaData.name}</p>
               <p className="text-lg text-slate-50 font-semibold">
                 {gameData.game.score.toLocaleString()} pts
               </p>
-              <p className="text-slate-400 text-sm">
+            </div>
+
+            <div className="flex flex-col md:flex-row md:items-center gap-4">
+              <p className="text-slate-400 text-sm ">
                 {new Date(gameData.game.date_score).toLocaleString("fr-FR")}
               </p>
               <Button
                 to={`/game/${gameData.game.id}`}
                 variant="info"
-                className="self-start mt-1"
+                className=""
               >
                 Détails
               </Button>
             </div>
+
           </li>
         ))}
       </ul>
